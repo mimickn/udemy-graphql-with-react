b30cb5475debc58771f4f420f281ef4aa8ac1b63
mimickn <mimickn1109@gmail.com>
2019-06-23 15:45:14 +0000

    titlize

--- src/App.js
+++ src/App.js
@@ -49,8 +49,13 @@ class App extends Component {
               if (loading) return 'loading...';
               if (error) return `Error! ${error.message}`
 
-              console.log({data});
-              return <div></div>;
+              console.log(data.search);
+
+              const search = data.search;
+              const repositoryCount = search.repositoryCount;
+              const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories';
+              const title = `Github Repositories Search Results - ${data.search.repositoryCount} ${repositoryUnit}`
+              return <h2>{title}</h2>;
             }
           }
         </Query>
