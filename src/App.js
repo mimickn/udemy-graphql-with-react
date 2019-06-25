79f618e5e476627d8d2449eace537afba27fd337
mimickn <mimickn1109@gmail.com>
2019-06-25 17:09:20 +0000

    test suggest

--- src/App.js
+++ src/App.js
@@ -3,14 +3,20 @@ import { ApolloProvider } from 'react-apollo';
 import { Query } from 'react-apollo';
 import client from './client'
 import { SEARCH_REPOSITORIES } from './graphql'
+import Autosuggest from "react-autosuggest";
 
 const DEFAULT_STATE = {
   first: 5,
   after: null,
   last: null,
   before: null,
-  query: "フロントエンドエンジニア"
+  query: "",
+  suggestions: [],
+  value: "",
 };
+
+const getSuggestionValue = suggestion => suggestion.node.name;
+const renderSuggestion = suggestion => <div>{suggestion.node.name}</div>;
 
 class App extends Component {
   constructor(props) {
@@ -18,9 +24,11 @@ class App extends Component {
     this.state = DEFAULT_STATE;
 
     this.handleChange = this.handleChange.bind(this);
+    this.suggestions = []
   }
 
   handleChange(event) {
+    console.log(event.target.value)
     this.setState({
       ...DEFAULT_STATE,
       query: event.target.value
@@ -31,15 +39,58 @@ class App extends Component {
     event.preventDefault()
   }
 
+  onSuggestionsFetchRequested = ({ value }) => {
+    // this.setState({
+    //   suggestions: getSuggestions(value)
+    // });
+  };
+
+  onSuggestionsClearRequested = () => {
+    this.setState({
+      suggestions: []
+    });
+    // this.suggestions = []
+  };
+
+  onChange = (event, { newValue }) => {
+    console.log(event.target.value)
+    console.log(newValue)
+    if ( event.target.value !== undefined ) {
+      this.setState({
+        value: newValue,
+        query: event.target.value
+      });
+    }
+    else {
+      this.setState({
+        value: newValue,
+        query: newValue
+      });
+    }
+  };
+
   render() {
-    const { query, first, last, before, after } = this.state;
+    const { query, first, last, before, after, suggestions, value } = this.state;
     console.log({query});
+
+    const inputProps = {
+      placeholder: "Type a programming language",
+      value,
+      onChange: this.onChange
+    };
 
     return (
       <ApolloProvider client={client} >
-        <form>
-          <input value={query} onChange={this.handleChange}/>
-        </form>
+
+        <Autosuggest
+          suggestions={this.state.suggestions}
+          // suggestions={this.suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={getSuggestionValue}
+          renderSuggestion={renderSuggestion}
+          inputProps={inputProps}
+        />
         <Query
           query={SEARCH_REPOSITORIES}
           variables={{ query, first, last, before, after }}
@@ -48,8 +99,13 @@ class App extends Component {
             ({ loading, error, data }) => {
               if (loading) return 'loading...';
               if (error) return `Error! ${error.message}`
-
+              console.log(data);
               console.log(data.search);
+
+              if ( this.state.suggestions !== data.search.edges ) {
+                this.setState({ suggestions: data.search.edges })
+              }
+              // this.suggestions = data.search.edges
 
               const search = data.search;
               const repositoryCount = search.repositoryCount;
